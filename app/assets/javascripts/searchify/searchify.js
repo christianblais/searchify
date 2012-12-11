14dd765a1489a07a03e6dd638675bded461b352c
Jodi Giordano <jodi@ephemeregames.com>
2012-12-11 16:57:36 -0500

    Bug fix & use of turbolinks.

--- app/assets/javascripts/searchify/searchify.js
+++ app/assets/javascripts/searchify/searchify.js
@@ -3,12 +3,13 @@
         return this.each(function() {
             $(this).autocomplete({
                 source: $(this).data("search-url"),
+                appendTo: 'body',
                 select: function (event, ui) {
                     if (select_url = $(this).data("select-url")) {
                         for (element in ui.item)
                             select_url = select_url.replace('\(' + element + '\)', ui.item[element]);
 
-                        window.location.href = select_url;
+                        Turbolinks.visit(select_url);
                     } else {
                         $(this).prev().val(ui.item.id);
                         $(this).data('value', ui.item.id)
